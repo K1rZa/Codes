8828c118464c8ba67c68f8c8e5d08e320dfb01a8
K1rZa <kirill.white7335@gmail.com>
2024-05-27 12:29:50 +0400

    modified:   HTML/TestCodes/Crossword/index.html 	modified:   HTML/TestCodes/Crossword/script.js

--- HTML/TestCodes/Crossword/script.js
+++ HTML/TestCodes/Crossword/script.js
@@ -1,38 +1,13 @@
-const CONTAINER = document.querySelector('container')
-const WORD_CLASS = document.getElementById('wordclass')
+const CONTAINER = document.querySelector('.container')
+const WORD_CLASS = document.querySelectorAll('.wordclass')
 
 const WORDS_ARRAY = [
-	{ number: 1, word: 'test1', orientation: 'vertical' },
-	{ number: 2, word: 'test2', orientation: 'horizontal' },
+	{ number: 1, word: 'test1', question: 'quest1' },
+	{ number: 2, word: 'test2', question: 'quest2' },
 ]
 
-for (let i = 0; i < WORDS_ARRAY.length; i++) {
-	if (WORDS_ARRAY[i].orientation === 'vertical') {
-		console.log('vertical')
-		WORD_CLASS.style.flexDirection = 'column'
-	} else if (WORDS_ARRAY[i].orientation === 'horizontal') {
-		console.log('horizontal')
-		WORD_CLASS.style.flexDirection = 'row'
-	} else {
-		console.log('word orientation ERROR')
-	}
-
-	let symbolArray = WORDS_ARRAY[i].word.split('')
-	let userSymbolArray = ['a', 'e', 's', 'w', 't']
-	let completedArray = []
-
-	console.log(symbolArray, userSymbolArray)
-
-	if (symbolArray.length === userSymbolArray.length) {
-		for (let j = 0; j < symbolArray.length; j++) {
-			if (symbolArray[j] === userSymbolArray[j]) {
-				completedArray.push('YES')
-			} else {
-				completedArray.push('ERROR')
-			}
-		}
-		console.log(completedArray.join(' '))
-	} else {
-		console.log('array length ERROR')
+if (WORD_CLASS.length === WORDS_ARRAY.length){
+	for (let i = 0; i < WORDS_ARRAY.length; i++){
+		console.log('hello')
 	}
 }
