a00e170f93288189a9f994041bf9d3ac308a9a6a
Kirill White <kirill.white7335@gmail.com>
2024-10-18 18:26:56 +0400

    new file:   HTML/Div_to_TextArea/index.html 	new file:   HTML/Div_to_TextArea/script.js 	new file:   HTML/Div_to_TextArea/style.css

---
+++ HTML/Div_to_TextArea/script.js
@@ -0,0 +1,24 @@
+const view = document.getElementById("view")
+
+view.addEventListener("dblclick", function () {
+        view.style.display = "none"
+
+        let edit = document.createElement("textarea")
+        edit.className = "edit"
+        edit.setAttribute("placeholder", "text")
+        edit.value = view.innerText
+    
+        view.after(edit)
+    
+        edit.addEventListener("blur", function () {
+            view.innerHTML = edit.value
+            edit.remove()
+            view.style.display = ""
+
+            let text = document.createElement("p")
+            text.className = "new_text"
+            text.innerHTML = view.innerHTML
+            view.innerHTML = ""
+            view.appendChild(text)
+        })
+})
