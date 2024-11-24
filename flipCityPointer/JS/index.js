b6c7d418957553b93938e44c54fbb1c1b9360cdb
Win_Kirill White <kirill.white7335@gmail.com>
2024-11-24 16:01:16 +0400

    modified:   flipCityPointer/JS/index.js

--- flipCityPointer/JS/index.js
+++ flipCityPointer/JS/index.js
@@ -3,28 +3,26 @@ const cityInput = document.getElementById("city-input")
 
 const flipScreenCount = 15
 
-function addFlipScreen (count){
-    for (let i = 0; i < count; i++){
+function addFlipScreen(count) {
+    for (let i = 0; i < count; i++) {
         let FS_div = document.createElement("div")
         FS_div.className = "city-pointer-part"
-        // FS_div.id = "flipScreen" + (i)
         cityPointerMain.appendChild(FS_div)
     }
 }
 
-addFlipScreen(flipScreenCount)
-
-function signOnScreen (count){
+function signOnScreen(count) {
     addEventListener("keypress", function(event) {
         var key = event.keyCode
-        if (key === 13){
+        if (key === 13) {
             let cityInputValue = cityInput.value
             let signArr = cityInputValue.split('')
 
             let get_FS_div = document.getElementsByClassName("city-pointer-part")
-            for (let i = 0; i < count; i++){
-                get_FS_div[i].innerHTML = signArr[i]
-                if (get_FS_div[i].innerHTML === "undefined"){
+            for (let i = 0; i < count; i++) {
+                if (signArr[i] !== undefined && /^[A-Za-zА-Яа-яЁё-]$/.test(signArr[i])) {
+                    displayLetter(get_FS_div[i], signArr[i])
+                } else {
                     get_FS_div[i].innerHTML = ""
                 }
             }
@@ -32,4 +30,19 @@ function signOnScreen (count){
     })
 }
 
+function displayLetter(divElement, targetLetter) {
+    const alphabet = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ-'
+    let index = 0
+
+    const interval = setInterval(() => {
+        divElement.textContent = alphabet[index % alphabet.length]
+        index++
+
+        if (divElement.textContent === targetLetter.toUpperCase()) {
+            clearInterval(interval)
+        }
+    }, 300)
+}
+
+addFlipScreen(flipScreenCount)
 signOnScreen(flipScreenCount)
