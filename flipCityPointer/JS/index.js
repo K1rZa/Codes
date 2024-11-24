3745ba5d0c817483fe3cf092194578a2dbb2124b
Win_Kirill White <kirill.white7335@gmail.com>
2024-11-24 13:31:11 +0400

    modified:   flipCityPointer/HTML/index.html 	modified:   flipCityPointer/JS/index.js

--- flipCityPointer/JS/index.js
+++ flipCityPointer/JS/index.js
@@ -7,23 +7,29 @@ function addFlipScreen (count){
     for (let i = 0; i < count; i++){
         let FS_div = document.createElement("div")
         FS_div.className = "city-pointer-part"
-        FS_div.id = "flipScreen" + (i)
+        // FS_div.id = "flipScreen" + (i)
         cityPointerMain.appendChild(FS_div)
     }
 }
 
 addFlipScreen(flipScreenCount)
 
-function getInputValue (){
+function signOnScreen (count){
     addEventListener("keypress", function(event) {
         var key = event.keyCode
         if (key === 13){
             let cityInputValue = cityInput.value
             let signArr = cityInputValue.split('')
-            console.log(signArr)  
+
+            let get_FS_div = document.getElementsByClassName("city-pointer-part")
+            for (let i = 0; i < count; i++){
+                get_FS_div[i].innerHTML = signArr[i]
+                if (get_FS_div[i].innerHTML === "undefined"){
+                    get_FS_div[i].innerHTML = ""
+                }
+            }
         }
     })
 }
 
-getInputValue()
-
+signOnScreen(flipScreenCount)
