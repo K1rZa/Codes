f3792145ed01ed8595120fab2edb253e4edb7b64
Win_Kirill White <kirill.white7335@gmail.com>
2024-12-14 15:30:51 +0400

    modified:   Preprocessor_Page/index.html 	modified:   Preprocessor_Page/script.js 	modified:   Preprocessor_Page/style.css 	modified:   Preprocessor_Page/style.css.map 	modified:   Preprocessor_Page/style.scss

--- Preprocessor_Page/script.js
+++ Preprocessor_Page/script.js
@@ -1,10 +1,151 @@
-var swiper = new Swiper(".home-gallery-main", {
+let swiper = new Swiper(".home-gallery-main", {
     direction: "horizontal",
     slidesPerView: 3,
-    spaceBetween: 9,
     mousewheel: true,
-    // pagination: {
-    //   el: ".swiper-pagination",
-    //   clickable: true,
-    // },
   });
+
+
+
+  class SvgLinkEffect {
+    constructor(effect) {
+      if (effect.config.random) {
+        this.randomizeArray(effect.nodes);
+      }
+  
+      effect.element.addEventListener("mouseenter", () => {
+        let reverse = effect.element.classList.contains("active") ? true : false;
+        effect.element.classList.toggle("active");
+        effect.handler(effect.nodes, effect.config, reverse);
+      });
+    }
+  
+    randomizeArray(array) {
+      for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+      }
+    }
+  }
+  
+  const svgBounceEffect = (nodes, config, reverse) => {
+    nodes.forEach((node, index) => {
+      const { duration, ease, y1, y2, offset } = config;
+      setTimeout(() => {
+        if (reverse) {
+          gsap.to(node, {
+            duration: duration,
+            ease: ease,
+            y: y1
+          });
+        } else {
+          gsap.to(node, {
+            duration: duration,
+            ease: ease,
+            y: y2
+          });
+        }
+      }, index * offset);
+    });
+  };
+  
+  const svgAlertnatingEffect = (nodes, config, reverse) => {
+    nodes.forEach((node, index) => {
+      const { duration, ease, offset } = config;
+      setTimeout(() => {
+        if (reverse) {
+          gsap.to(node, {
+            duration: duration,
+            ease: ease,
+            y: index % 2 === 0 ? -44 : 44
+          });
+        } else {
+          gsap.to(node, {
+            duration: duration,
+            ease: ease,
+            y: index % 2 === 0 ? 44 : -44
+          });
+        }
+      }, index * offset);
+    });
+  };
+  
+  document.addEventListener("DOMContentLoaded", (event) => {
+  
+    const randomStepLinkHome = document.getElementById("header-nav-home");
+    const randomStepLinkEffectHome = {
+      element: randomStepLinkHome,
+      handler: svgBounceEffect,
+      nodes: [...randomStepLinkHome.querySelectorAll("rect")],
+      config: {
+        offset: 10,
+        duration: 0.8,
+        random: true,
+        ease: "steps(5)",
+        y1: -20,
+        y2: 44
+      }
+    };
+    const randomStepLinkInfo = document.getElementById("header-nav-info");
+    const randomStepLinkEffectInfo = {
+      element: randomStepLinkInfo,
+      handler: svgBounceEffect,
+      nodes: [...randomStepLinkInfo.querySelectorAll("rect")],
+      config: {
+        offset: 10,
+        duration: 0.8,
+        random: true,
+        ease: "steps(5)",
+        y1: -20,
+        y2: 44
+      }
+    };
+    const randomStepLinkTournament1 = document.getElementById("header-nav-tournament-before");
+    const randomStepLinkEffectTournament1 = {
+      element: randomStepLinkTournament1,
+      handler: svgBounceEffect,
+      nodes: [...randomStepLinkTournament1.querySelectorAll("rect")],
+      config: {
+        offset: 10,
+        duration: 0.8,
+        random: true,
+        ease: "steps(5)",
+        y1: -20,
+        y2: 44
+      }
+    };
+    const randomStepLinkTournament2 = document.getElementById("header-nav-tournament-after");
+    const randomStepLinkEffectTournament2 = {
+      element: randomStepLinkTournament2,
+      handler: svgBounceEffect,
+      nodes: [...randomStepLinkTournament2.querySelectorAll("rect")],
+      config: {
+        offset: 10,
+        duration: 0.8,
+        random: true,
+        ease: "steps(5)",
+        y1: -20,
+        y2: 44
+      }
+    };
+    const randomStepLinkRegister = document.getElementById("header-nav-register");
+    const randomStepLinkEffectRegister = {
+      element: randomStepLinkRegister,
+      handler: svgBounceEffect,
+      nodes: [...randomStepLinkRegister.querySelectorAll("rect")],
+      config: {
+        offset: 10,
+        duration: 0.8,
+        random: true,
+        ease: "steps(5)",
+        y1: -20,
+        y2: 44
+      }
+    };
+
+    new SvgLinkEffect(randomStepLinkEffectHome);
+    new SvgLinkEffect(randomStepLinkEffectInfo);
+    new SvgLinkEffect(randomStepLinkEffectTournament1);
+    new SvgLinkEffect(randomStepLinkEffectTournament2);
+    new SvgLinkEffect(randomStepLinkEffectRegister);
+  });
+  
