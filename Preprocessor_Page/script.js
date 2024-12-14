c43f7fcab9edf8ca8e1926dc6c25d41635f3f762
Win_Kirill White <kirill.white7335@gmail.com>
2024-12-14 14:39:37 +0400

    modified:   Preprocessor_Page/index.html 	modified:   Preprocessor_Page/script.js 	modified:   Preprocessor_Page/style.css 	modified:   Preprocessor_Page/style.css.map 	modified:   Preprocessor_Page/style.scss

---
+++ Preprocessor_Page/script.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".home-gallery-main", {
+    direction: "horizontal",
+    slidesPerView: 3,
+    spaceBetween: 9,
+    mousewheel: true,
+    // pagination: {
+    //   el: ".swiper-pagination",
+    //   clickable: true,
+    // },
+  });
