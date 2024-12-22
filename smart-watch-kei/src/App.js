119b9132bf724c772c5204e0d12be140c32697c1
Win_Kirill White <kirill.white7335@gmail.com>
2024-12-22 13:54:56 +0400

    modified:   smart-watch-kei/src/App.js 	modified:   smart-watch-kei/src/App.scss

--- smart-watch-kei/src/App.js
+++ smart-watch-kei/src/App.js
@@ -1,12 +1,35 @@
-import './App.scss';
+import "./App.scss"
+import React, { useRef } from 'react';
 
 function App() {
+  const buttonsScreen_Ref = useRef(null);
+  const scheduleScreen_Ref = useRef(null);
+  const changesScreen_Ref = useRef(null);
+
+  function goToSchedule() {
+    if (scheduleScreen_Ref.current && changesScreen_Ref.current && buttonsScreen_Ref.current) {
+      scheduleScreen_Ref.current.style.display = 'flex';
+      changesScreen_Ref.current.style.display = 'none';
+      buttonsScreen_Ref.current.style.display = 'none';
+    }
+  }
+
+  function goToChanges() {
+    if (changesScreen_Ref.current && scheduleScreen_Ref.current && buttonsScreen_Ref.current) {
+      changesScreen_Ref.current.style.display = 'flex';
+      scheduleScreen_Ref.current.style.display = 'none';
+      buttonsScreen_Ref.current.style.display = 'none';
+    }
+  }
+
   return (
     <div className="App">
-      <main>
-        <input type="button" value="Расписание"></input>
-        <input type="button" value="Изменения"></input>
-      </main>
+      <div ref={buttonsScreen_Ref} id="buttonsScreen">
+        <button id="scheduleMainButton" onClick={goToSchedule}>Расписание</button>
+        <button id="changesMainButton" onClick={goToChanges}>Изменения</button>
+      </div>
+      <div ref={scheduleScreen_Ref} id="scheduleScreen"></div>
+      <div ref={changesScreen_Ref} id="changesScreen"></div>
     </div>
   );
 }
